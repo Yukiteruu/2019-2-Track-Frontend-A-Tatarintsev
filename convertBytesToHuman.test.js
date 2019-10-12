e2827d8a56b41f48857cbcffd9a5ce9283b440fc
сучара <lagrangelui@gmail.com>
2019-10-12 04:41:24 +0300

    new commit

--- convertBytesToHuman.test.js
+++ convertBytesToHuman.test.js
@@ -11,31 +11,19 @@ import convertBytesToHuman from './convertBytesToHuman';
 
 test('Возвращает false для неправильного типа данных', () => {
   expect(convertBytesToHuman(-1)).toBe(false)
-  // ...
+  expect(convertBytesToHuman("string")).toBe(false)
 });
 
 test('Возвращает корректное значение для чисел', () => {
   expect(convertBytesToHuman(1024)).toBe("1 KB")
-  // ...
-});
-test('Возвращает корректное значение для чисел', () => {
   expect(convertBytesToHuman(5)).toBe("5 B")
-  // ...
-});
-test('Возвращает false для неправильного типа данных', () => {
-  expect(convertBytesToHuman("string")).toBe(false)
-  // ...
-});
-test('Возвращает корректное значение для чисел', () => {
   expect(convertBytesToHuman(123123123)).toBe("117.42 MB")
-  // ...
-});
-test('Возвращает корректное значение для чисел', () => {
   expect(convertBytesToHuman(123123123123)).toBe("114.67 GB")
-  // ...
-});
-test('Возвращает корректное значение для чисел', () => {
   expect(convertBytesToHuman(123123123123123)).toBe("111.98 TB")
-  // ...
 });
-// другая группа проверок
+
+
+
+
+
+
